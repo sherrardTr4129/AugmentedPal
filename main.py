642289ef9f17e23818eb6be4b618526e9c332e56
Trevor Sherrard <sherrardtr@gmail.com>
2016-01-26 00:39:44 -0500

    made changes to main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import cv2
 from detection import Detection
 from render import Effects
 
+
+
+detect = Detection()
+cap = cv2.VideoCapture(0)
+ren = Effects()
+while True:
+     ret, frame = cap.read()
+     #detect.is_item_detected_in_image("faceDetection.xml", frame)
+     ren.render(frame)
+     cv2.imshow("frame", frame)
+     cv2.waitKey(1)
 
