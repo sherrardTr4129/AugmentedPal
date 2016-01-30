802a36511eb6b8451b6a0db4988257230c5984c3
Nick Mosher <nicholastmosher@gmail.com>
2016-01-30 02:20:28 -0500

    changed formating

--- detection.py
+++ detection.py
@@ -3,18 +3,18 @@ import cv2
 
 
 class Detection(object):
-     def Detect(self, Haar1, Haar2, img):
-         cascade1 = cv2.CascadeClassifier(Haar1)
-         cascade2 = cv2.CascadeClassifier(Haar2)
-         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-         item1 = cascade1.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (50,50))
-         item2 = cascade2.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (50,50))
-         for (x,y,w,h) in item1:
-              cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-         for (x,y,w,h) in item2:
-              cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),2)
-         if(len(item1) == 0 and len(item2) == 0):
-              return False, False
-         else:
-	      return len(item1) > 0, len(item2) > 0
 
+    def Detect(self, Haar1, Haar2, img):
+        cascade1 = cv2.CascadeClassifier(Haar1)
+        cascade2 = cv2.CascadeClassifier(Haar2)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        item1 = cascade1.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (50, 50))
+        item2 = cascade2.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (50, 50))
+        for (x, y, w, h) in item1:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        for (x, y, w, h) in item2:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        if(len(item1) == 0 and len(item2) == 0):
+            return False, False
+        else:
+            return len(item1) > 0, len(item2) > 0
--- render.py
+++ render.py
@@ -1,69 +1,72 @@
 import cv2
 import numpy as np
-  
+
+
 class Effects(object):
-    
-    def render(self, CSH, hammer, other ,image, axisBot, axisTop, axisCol, axisS1, axisS2, axisS3, axisS4, axisS5, axisH1, axisH2):
-  
+
+    def render(self, CSH, hammer, other, image, axisBot, axisTop, axisCol, axisS1, axisS2, axisS3, axisS4, axisS5, axisH1, axisH2):
+
         # load calibration data
         with np.load('webcam_calibration_ouput.npz') as X:
-            mtx, dist, _, _ = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
-  
+            mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+
         # set up criteria, object points and axis
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-          
-        objp = np.zeros((6*7,3), np.float32)
-        objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-  
+        criteria = (cv2.TERM_CRITERIA_EPS +
+                    cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+        objp = np.zeros((6 * 7, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+
         # find grid corners in image
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, (7,9), None)
+        ret, corners = cv2.findChessboardCorners(gray, (7, 9), None)
 
-  
         if ret == True:
-            cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+            cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             rvecs, tvecs, _ = cv2.solvePnPRansac(objp, corners, mtx, dist)
-	    if CSH == True:
+            if CSH == True:
                 # project 3D points to image plane
-            	imgpts, _ = cv2.projectPoints(axisBot, rvecs, tvecs, mtx, dist)
-            	imgpts1, _ = cv2.projectPoints(axisTop, rvecs, tvecs, mtx, dist)
-            	imgpts2, _ = cv2.projectPoints(axisCol, rvecs, tvecs, mtx, dist)
-            	imgpts3, _ = cv2.projectPoints(axisS1, rvecs, tvecs, mtx, dist)
-            	imgpts4, _ = cv2.projectPoints(axisS2, rvecs, tvecs, mtx, dist)
-            	imgpts5, _ = cv2.projectPoints(axisS3, rvecs, tvecs, mtx, dist)
-            	imgpts6, _ = cv2.projectPoints(axisS4, rvecs, tvecs, mtx, dist)
-            	imgpts7, _ = cv2.projectPoints(axisS5, rvecs, tvecs, mtx, dist)
-            	imgpts8, _ = cv2.projectPoints(axisH1, rvecs, tvecs, mtx, dist)
-            	imgpts9, _ = cv2.projectPoints(axisH2, rvecs, tvecs, mtx, dist)
+                imgpts, _ = cv2.projectPoints(axisBot, rvecs, tvecs, mtx, dist)
+                imgpts1, _ = cv2.projectPoints(
+                    axisTop, rvecs, tvecs, mtx, dist)
+                imgpts2, _ = cv2.projectPoints(
+                    axisCol, rvecs, tvecs, mtx, dist)
+                imgpts3, _ = cv2.projectPoints(axisS1, rvecs, tvecs, mtx, dist)
+                imgpts4, _ = cv2.projectPoints(axisS2, rvecs, tvecs, mtx, dist)
+                imgpts5, _ = cv2.projectPoints(axisS3, rvecs, tvecs, mtx, dist)
+                imgpts6, _ = cv2.projectPoints(axisS4, rvecs, tvecs, mtx, dist)
+                imgpts7, _ = cv2.projectPoints(axisS5, rvecs, tvecs, mtx, dist)
+                imgpts8, _ = cv2.projectPoints(axisH1, rvecs, tvecs, mtx, dist)
+                imgpts9, _ = cv2.projectPoints(axisH2, rvecs, tvecs, mtx, dist)
 
-  
-            	# draw cube
-            	self._draw_cube(image, imgpts)
-            	self._draw_cube(image, imgpts1)
-            	self._draw_cube(image, imgpts2)
-	    	self._draw_cube(image, imgpts3)
-	    	self._draw_cube(image, imgpts4)
-	    	self._draw_cube(image, imgpts5)
-	    	self._draw_cube(image, imgpts6)
-	    	self._draw_cube(image, imgpts7)
-	    	self._draw_cube(image, imgpts8)
-	    	self._draw_cube(image, imgpts9)
-	    	return image
-	    elif hammer == True:
-            	imgptsOther, _ = cv2.projectPoints(other, rvecs, tvecs, mtx, dist)
-            	self._draw_cube(image, imgptsOther)
-		return image
+                # draw cube
+                self._draw_cube(image, imgpts)
+                self._draw_cube(image, imgpts1)
+                self._draw_cube(image, imgpts2)
+                self._draw_cube(image, imgpts3)
+                self._draw_cube(image, imgpts4)
+                self._draw_cube(image, imgpts5)
+                self._draw_cube(image, imgpts6)
+                self._draw_cube(image, imgpts7)
+                self._draw_cube(image, imgpts8)
+                self._draw_cube(image, imgpts9)
+                return image
+            elif hammer == True:
+                imgptsOther, _ = cv2.projectPoints(
+                    other, rvecs, tvecs, mtx, dist)
+                self._draw_cube(image, imgptsOther)
+                return image
+
     def _draw_cube(self, img, imgpts):
-        imgpts = np.int32(imgpts).reshape(-1,2)
-  
+        imgpts = np.int32(imgpts).reshape(-1, 2)
+
         # draw floor
-        cv2.drawContours(img, [imgpts[:4]],-1,(0,0,0),3)
-  
+        cv2.drawContours(img, [imgpts[:4]], -1, (0, 0, 0), 3)
+
         # draw pillars
-        for i,j in zip(range(4),range(4,8)):
-            cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(0),3)
-  
+        for i, j in zip(range(4), range(4, 8)):
+            cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]), (0), 3)
+
         # draw roof
-        cv2.drawContours(img, [imgpts[4:8]],-1,(0,0,0),3)
-	return img
-    
+        cv2.drawContours(img, [imgpts[4:8]], -1, (0, 0, 0), 3)
+        return img
